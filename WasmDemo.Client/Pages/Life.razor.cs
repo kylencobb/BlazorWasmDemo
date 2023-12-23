11f52d571d4afee2bba11198147bfb6a61d8fa84
shepherd <kylencobb@gmail.com>
2023-12-23 17:06:39 -0500

    Adding Conway's Game of Life.

---
+++ WasmDemo.Client/Pages/Life.razor.cs
@@ -0,0 +1,100 @@
+﻿namespace WasmDemo.Client.Pages
+{
+    public partial class Life
+    {
+        private bool[,] _cells;
+        private bool _isPlaying;
+        private int _iteration;
+        private int _population;
+        private int _iterateDelayMs;
+
+        protected override Task OnInitializedAsync()
+        {
+            _cells = new bool[100, 100];
+            _isPlaying = false;
+            _iteration = 0;
+            _population = 0;
+            _iterateDelayMs = 1;
+
+            return base.OnInitializedAsync();
+        }
+
+        private void Flip(int rowIndex, int columnIndex, bool isIncrementing = true)
+        {  
+            bool flippedValue = !_cells[rowIndex, columnIndex];
+
+            _cells[rowIndex, columnIndex] = flippedValue;
+
+            if (flippedValue)
+            {
+                _population++;
+                return;
+            }
+
+            _population--;
+        }
+
+        private async Task Play()
+        {
+            _isPlaying = true;
+
+            while (_isPlaying)
+            {
+                Iterate();
+                StateHasChanged();
+                await Task.Delay(_iterateDelayMs);
+            }
+        }
+
+        private void Iterate()
+        {
+            _iteration++;
+
+            List<(int, int)> cellsForFlip = new List<(int, int)>();
+
+            for (int i = 0; i < _cells.GetLength(0); i++)
+            {
+                for (int j = 0;  j < _cells.GetLength(1); j++)
+                {
+                    int activeNeighborCount = GetNeighborValues(i, j).Count(x => x == true);
+
+                    if (_cells[i, j] && (activeNeighborCount == 2 || activeNeighborCount == 3))
+                        continue;
+
+                    if (_cells[i, j] && activeNeighborCount < 2)
+                        cellsForFlip.Add((i, j));
+
+                    if (_cells[i, j] && activeNeighborCount > 3)
+                        cellsForFlip.Add((i, j));
+
+                    if (!_cells[i, j] && activeNeighborCount == 3)
+                        cellsForFlip.Add((i, j));
+                }
+            }
+
+            foreach ((int, int) cell in cellsForFlip)
+            {
+                Flip(cell.Item1, cell.Item2);
+            }
+        }
+
+        private List<bool> GetNeighborValues(int rowIndex, int columnIndex)
+        {
+            List<bool> neighbors = new List<bool>();
+
+            if (columnIndex - 1 >= 0) neighbors.Add(_cells[rowIndex, columnIndex - 1]);
+            if (rowIndex - 1 >= 0) neighbors.Add(_cells[rowIndex - 1, columnIndex]);
+
+            if (columnIndex + 1 < 100) neighbors.Add(_cells[rowIndex, columnIndex + 1]);
+            if (rowIndex + 1 < 100) neighbors.Add(_cells[rowIndex + 1, columnIndex]);
+
+            if (columnIndex - 1 >= 0 && rowIndex + 1 < 100) neighbors.Add(_cells[rowIndex + 1, columnIndex - 1]);
+            if (columnIndex + 1 < 100 && rowIndex - 1 >= 0) neighbors.Add(_cells[rowIndex - 1, columnIndex + 1]);
+            
+            if (columnIndex - 1 >= 0 && rowIndex - 1 >= 0) neighbors.Add(_cells[rowIndex - 1, columnIndex - 1]);
+            if (columnIndex + 1 < 100 && rowIndex + 1 < 100) neighbors.Add(_cells[rowIndex + 1, columnIndex + 1]);
+
+            return neighbors;
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,14 +1,17 @@
 # Blazor Wasm Demo
-This project was created in a few hours during Thanksgiving to demonstrate proficiency in Blazor WebAssembly. This is not intended to be a robust demonstration. I am excited by the prospect of working with Blazor.
+This project was created to demonstrate proficiency in Blazor WebAssembly.
 
 ## Local Setup
 To run this application locally, use Multiple Startup Projects in Visual Studio (starting both the Api and Client projects).
 
 ## Authentication
-You can login using any credentials. The Api provisions a Json Web Token with an 'Administrator' role which can be used to view a protected table on the homepage and an Admin page which allows you to manage a list of people.
+You can login using any credentials. The Api provisions a Json Web Token with an 'Administrator' role which can be used to view a protected table on the homepage, an Admin page which allows you to manage a list of people, and a page on which you can play [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life).
 
 ## Home
 Once logged in, the homepage will display a TableSearchSort component filled with dummy data. You can search for any text within the table to filter the list of people. You can also sort the table by selecting the table header for the column you wish to sort.
 
 ## Admin Page
 Contains a small form for adding a new person to an existing list of people.
+
+## Conway's Game of Life
+An interactive page where users can play [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life). Select cells in a grid to set the initial population. Then select 'Play' to watch the game play out.
