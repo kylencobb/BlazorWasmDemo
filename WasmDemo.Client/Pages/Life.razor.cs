6dbc91a5a03c08dbd6207fcf9cb818d89693c59a
shepherd <kylencobb@gmail.com>
2023-12-27 16:02:43 -0500

    Minor performance improvements for game of life. Small ui edits.

--- WasmDemo.Client/Pages/Life.razor.cs
+++ WasmDemo.Client/Pages/Life.razor.cs
@@ -10,20 +10,8 @@ namespace WasmDemo.Client.Pages
         private int _population;
         private int _iterateDelayMs;
         private List<string> _presetGrids = new List<string> { "Gosper Glider", "Simkin Glider" };
-        private string _playIconClass
-        {
-            get
-            {
-                return _isPlaying ? "bi-play-fill text-success" : "bi-play text-white";
-            }
-        }
-        private string _stopIconClass
-        {
-            get
-            {
-                return _isPlaying ? "bi-stop text-white" : "bi-stop-fill text-danger";
-            }
-        }
+        private string _playIconClass { get { return _isPlaying ? "bi-play-fill text-success" : "bi-play text-white"; } }
+        private string _stopIconClass { get { return _isPlaying ? "bi-stop text-white" : "bi-stop-fill text-danger"; } }
 
         protected override Task OnInitializedAsync()
         {
@@ -36,24 +24,17 @@ namespace WasmDemo.Client.Pages
             return base.OnInitializedAsync();
         }
 
-        private void Flip(int rowIndex, int columnIndex, bool setTrue = false)
+        private void Flip(int rowIndex, int columnIndex)
         {
-            bool isPopulated = setTrue;
-            
-            if (!setTrue)
-            {
-                isPopulated = !_cells[rowIndex, columnIndex];
-            }
+            _cells[rowIndex, columnIndex] = !_cells[rowIndex, columnIndex];
 
-            _cells[rowIndex, columnIndex] = isPopulated;
-
-            if (isPopulated)
+            if (_cells[rowIndex, columnIndex] == false)
             {
-                _population++;
+                _population--;
                 return;
             }
 
-            _population--;
+            _population++;
         }
 
         private async Task Play()
@@ -65,7 +46,7 @@ namespace WasmDemo.Client.Pages
                 Iterate();
                 StateHasChanged();
                 await Task.Delay(_iterateDelayMs);
-            }
+            } 
         }
 
         private void Iterate()
@@ -128,6 +109,9 @@ namespace WasmDemo.Client.Pages
                 case "Gosper Glider":
                     SetGosperGlider();
                     break;
+                case "Simkin Glider":
+                    SetSimkinGlider();
+                    break;
                 default:
                     break;
             }
@@ -135,42 +119,82 @@ namespace WasmDemo.Client.Pages
 
         private void SetGosperGlider()
         {
+            Flip(10, 55);
+            Flip(11, 53);
+            Flip(11, 55);
+            Flip(12, 43);
+            Flip(12, 44);
+            Flip(12, 51);
+            Flip(12, 52);
+            Flip(12, 65);
+            Flip(12, 66);
+            Flip(13, 42);
+            Flip(13, 46);
+            Flip(13, 51);
+            Flip(13, 52);
+            Flip(13, 65);
+            Flip(13, 66);
+            Flip(14, 31);
+            Flip(14, 32);
+            Flip(14, 41);
+            Flip(14, 47);
+            Flip(14, 51);
+            Flip(14, 52);
+            Flip(15, 31);
+            Flip(15, 32);
+            Flip(15, 41);
+            Flip(15, 45);
+            Flip(15, 47);
+            Flip(15, 48);
+            Flip(15, 53);
             Flip(15, 55);
-            Flip(16, 53);
+            Flip(16, 41);
+            Flip(16, 47);
             Flip(16, 55);
-            Flip(17, 43);
-            Flip(17, 44);
-            Flip(17, 51);
-            Flip(17, 52);
-            Flip(17, 65);
-            Flip(17, 66);
-            Flip(18, 42);
-            Flip(18, 46);
-            Flip(18, 51);
-            Flip(18, 52);
-            Flip(18, 65);
-            Flip(18, 66);
-            Flip(19, 31);
-            Flip(19, 32);
-            Flip(19, 41);
-            Flip(19, 47);
-            Flip(19, 51);
-            Flip(19, 52);
-            Flip(20, 31);
-            Flip(20, 32);
-            Flip(20, 41);
-            Flip(20, 45);
-            Flip(20, 47);
-            Flip(20, 48);
-            Flip(20, 53);
-            Flip(20, 55);
-            Flip(21, 41);
-            Flip(21, 47);
-            Flip(21, 55);
-            Flip(22, 42);
-            Flip(22, 46);
-            Flip(23, 43);
-            Flip(23, 44);
+            Flip(17, 42);
+            Flip(17, 46);
+            Flip(18, 43);
+            Flip(18, 44);
+        }
+
+        private void SetSimkinGlider()
+        {
+            Flip(12, 35);
+            Flip(12, 36);
+            Flip(12, 42);
+            Flip(12, 43);
+            Flip(13, 35);
+            Flip(13, 36);
+            Flip(13, 42);
+            Flip(13, 43);
+            Flip(15, 39);
+            Flip(15, 40);
+            Flip(16, 39);
+            Flip(16, 40);
+            Flip(21, 57);
+            Flip(21, 58);
+            Flip(21, 60);
+            Flip(21, 61);
+            Flip(22, 56);
+            Flip(22, 62);
+            Flip(23, 56);
+            Flip(23, 63);
+            Flip(23, 66);
+            Flip(23, 67);
+            Flip(24, 56);
+            Flip(24, 57);
+            Flip(24, 58);
+            Flip(24, 62);
+            Flip(24, 66);
+            Flip(24, 67);
+            Flip(25, 61);
+            Flip(29, 55);
+            Flip(29, 56);
+            Flip(30, 55);
+            Flip(31, 56);
+            Flip(31, 57);
+            Flip(31, 58);
+            Flip(32, 58);
         }
     }
 }
